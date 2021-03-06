2df8898797904932a78d9bebc407cc6509c8431d
Kolpashchikov Denis <kolpashikov.denis@gmail.com>
2021-03-06 14:12:22 +0400

    Изменение №1

--- app01.py
+++ app01.py
@@ -13,6 +13,15 @@ class MyApp:
     def out(self):
         print(f'{self.FirstName} {self.LastName} {self.Age}')
 
+    def get_firstname(self):
+        return self.FirstName
+
+    def get_lastname(self):
+        return self.LastName
+
+    def get_age(self):
+        return self.Age
+
 
 
 # -------- End ---------------
@@ -20,3 +29,6 @@ class MyApp:
 if __name__ == "__main__":
     m = MyApp(sys.argv[1], sys.argv[2], sys.argv[3])
     m.out()
+    print(m.get_firstname())
+    print(m.get_lastname())
+    print(m.get_age())
