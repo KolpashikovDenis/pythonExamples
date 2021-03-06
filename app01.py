61dcb5a804cbb1e20dfb111514d4155298dafb14
Kolpashchikov Denis <kolpashikov.denis@gmail.com>
2021-03-06 14:00:17 +0400

    Добавлен класс

--- app01.py
+++ app01.py
@@ -1,4 +1,22 @@
 #!/bin/python3
 import os, sys
 
-print(sys.version)
+
+# ------- Class Definition ---------
+class MyApp:
+
+    def __init__ (self, first_name, last_name, age):
+        self.FirstName = first_name
+        self.LastName = last_name
+        self.Age = age
+
+    def out(self):
+        print(f'{self.FirstName} {self.LastName} {self.Age}')
+
+
+
+# -------- End ---------------
+
+if __name__ == "__main__":
+    m = MyApp(sys.argv[1], sys.argv[2], sys.argv[3])
+    m.out()
